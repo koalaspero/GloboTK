79d90523e841cabbc38667456772e7252535f49c
Diego Eduardo Arteaga Chavez <diedarte@espol.edu.ec>
2021-11-11 22:05:53 -0500

    Seccion Explorar

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,27 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollPos = currentTop;
     });
 })
+
+function initialize() {
+    var earth = new WE.map('earth_div');
+    WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(earth);
+
+    fetch("data/countries.json")
+    .then(response => response.json())
+    .then(data => {
+        for(let i = 0; i < Object.keys(data).length ; i++){
+            let htmLine = "<b>"+ data[i]['name'] + "</b><br>Artistas más escuchados<br><a href='https://spotifycharts.com/regional/"+ data[i]['country'].toLowerCase()+"/daily/latest'>click here</a>";
+            let latitude = data[i]['latitude'];
+            let longitude = data[i]['longitude'];
+            var marker = WE.marker([latitude, longitude]).addTo(earth);
+            marker.bindPopup(htmLine, {maxWidth: 120, closeButton: true});
+        }
+    })
+    .catch(console.error);
+
+    var markerCustom = WE.marker([50, -9], '/img/logo-webglearth-white-100.png', 100, 24).addTo(earth);
+
+    earth.setView([-1.6, -78], 4);
+}
+
+
