920b8161e6ae7f220ea33fae7ffbd0953a816b43
rpcedill <73241423+rpcedill@users.noreply.github.com>
2021-12-02 22:54:55 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -102,15 +102,18 @@ const peticion = () => {
  
 
 function initMap() {
-  var map = L.map('map').setView([-2.1449, -79.9676], 16);
+	var map = L.map('map').setView([-2.1449, -79.9676], 15);
 
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+	var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+    osm.addTo(map);
 
 	L.marker([-2.1449, -79.9676]).addTo(map)
 		.bindPopup('FIEC ESPOL')
 		.openPopup();
+		
+	L.control.locate().addTo(map);
 }
 
  
